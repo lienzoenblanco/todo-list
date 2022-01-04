9dffcafa29828a2f06f37172b5884d3c46eb8eec
lienzoenblanco <raquelgarciatrives@gmail.com>
2022-01-04 12:07:03 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,40 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let globalTodos = [];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function printTodos() {
+  const ul = document.querySelector(".todo-list");
+  let content = `<li><input id="data" type="text" placeholder="What needs to be done?" onkeyup="todoInputPressed(event)" /></li>`;
+  globalTodos.forEach(function(item, position) {
+    content =
+      content +
+      `<li>${item}<button class="button-remove" onClick='remove(${position})'>🗑</button></li>`;
+  });
+  ul.innerHTML = content;
+  const inputData = document.querySelector("#data");
+  inputData.focus();
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function addOneTodo(label) {
+  if (label.trim().length !== 0) {
+    globalTodos.push(label);
+  }
+  printTodos();
+}
+
+function todoInputPressed(event) {
+  if (event.keyCode != 13) {
+    return;
+  }
+  addOneTodo(event.target.value);
+}
+
+function remove(position) {
+  globalTodos = globalTodos.filter(function(item, index) {
+    return index != position;
+  });
+  printTodos();
+}
+
+window.addOneTodo = addOneTodo;
+window.todoInputPressed = todoInputPressed;
+window.remove = remove;
+printTodos();
